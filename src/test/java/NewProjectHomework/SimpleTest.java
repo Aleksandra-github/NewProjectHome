1bd529c07d43602675a4d93ecf6ff5ce05e35119
Aleksandra-github <akovenkoaleksandra68@gmail.com>
2021-04-19 15:13:43 +0300

    .gitignore was added

--- src/test/java/NewProjectHomework/SimpleTest.java
+++ src/test/java/NewProjectHomework/SimpleTest.java
@@ -1,4 +1,13 @@
 package NewProjectHomework;
+import org.testng.annotations.Test;
 
 public class SimpleTest {
+    WebDriver driver;
+    @Test
+    public void checkMethod() {
+        Sustem.setProperty("webdriver.chrom.driver", "C:\\Users\\admin\\Desktop\\driver\\chromedriver.exe");
+        driver =  new ChromeDriver ();
+        driver.get("http://151.80.70.42:3000/");
+    }
+
 }
