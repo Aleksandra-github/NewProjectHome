3d90e6287947e1b4ef9afb1b838ce88d1814b102
Aleksandra-github <akovenkoaleksandra68@gmail.com>
2021-04-21 23:19:41 +0300

    Where 2 files was chendged

--- src/test/java/NewProjectHomework/SimpleTest.java
+++ src/test/java/NewProjectHomework/SimpleTest.java
@@ -1,13 +1,35 @@
 package NewProjectHomework;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import java.util.UUID;
+import org.testng.annotations.AfterClass;
+
+
 
 public class SimpleTest {
     WebDriver driver;
     @Test
     public void checkMethod() {
-        Sustem.setProperty("webdriver.chrom.driver", "C:\\Users\\admin\\Desktop\\driver\\chromedriver.exe");
-        driver =  new ChromeDriver ();
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\Desktop\\driver\\chromedriver.exe");
+        driver = new ChromeDriver();
         driver.get("http://151.80.70.42:3000/");
+        driver.findElement(By.cssSelector(".mdi-content-add")).click();
+        driver.findElement(By.cssSelector("div.main-content #icon_telephone")).clear();
+        driver.findElement(By.cssSelector("input[id=\"icon_prefix\"]")).clear();
+        driver.findElement(By.cssSelector("input[id=\"icon_prefix\"]")).sendKeys(UUID.randomUUID().toString());
+        long timeInMillis = System.currentTimeMillis() % 1000000000000L;
+        driver.findElement(By.cssSelector("div.main-content #icon_telephone")).sendKeys(((Long) timeInMillis).toString());
+        driver.findElement(By.cssSelector("div.col.s9 a.save-btn")).click();
+
+
+    }
+        @AfterClass
+                public void afterAll() {
+        driver.quit();
+
     }
 
 }
